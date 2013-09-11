c8262b76b0b6a4e04663fad984a173e949b54a17
latitude98 <latitude98@gmail.com>
2013-09-11 14:43:51 -0700

    count_word complete

--- Lab2.rb
+++ Lab2.rb
@@ -21,5 +21,16 @@ end
 
 
 def count_words(string)
-	
+	temp_str = string.downcase #=> get rid of non-words
+	wordarray = temp_str.scan(/\w+/) #=> put words from string into array
+	myhash = Hash.new(0) #=> new hash
+	wordarray.each {|word| myhash[word] += 1 } #=> puts elements of array into hash as keys and keeps track of how many times the word appears
+	return myhash
+end
+
+#=> count_words test: puts count_words("A man, a plan, a canal -- Panama")
+#=> puts count_words("Doo bee doo bee doo")
+
+def combine_anagrams(words)
+
 end
