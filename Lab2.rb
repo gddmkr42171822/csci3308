74f3062d9d359a71fca24cd8bfba8a0bb9b5af31
latitude98 <latitude98@gmail.com>
2013-09-11 13:58:30 -0700

    palindrome done

--- Lab2.rb
+++ Lab2.rb
@@ -9,3 +9,17 @@ class HelloWorldClass
 end
 hello = HelloWorldClass.new("Bob")
 hello.sayHi
+
+
+# Part2: Strings
+def palindrome?(string)
+	temp_str = string.downcase.gsub(/\W/, "")
+	temp_str == temp_str.reverse 
+end
+
+#=> palindrom test: puts palindrome?("Madam, I'm Adam!")
+
+
+def count_words(string)
+	
+end
