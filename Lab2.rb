f293171e4e5d7405017cad10656dbbd055591da0
latitude98 <latitude98@gmail.com>
2013-09-11 19:19:40 -0700

    rps

--- Lab2.rb
+++ Lab2.rb
@@ -31,6 +31,41 @@ end
 #=> count_words test: puts count_words("A man, a plan, a canal -- Panama")
 #=> puts count_words("Doo bee doo bee doo")
 
+# Part3: Rock Paper Scissors
+class WrongNumberOfPlayersError < StandardError ; end
+class NoSuchStrategyError < StandardError ; end
+
+def rps_game_winner(game)
+	raise WrongNumberOfPlayersError unless game.length == 2
+	p1strategy = game[0][1].downcase
+	p2strategy = game[1][1].downcase
+	raise NoSuchStrategyError unless p1strategy == "r" || p1strategy == "p" || p1strategy == "s"
+	raise NoSuchStrategyError unless p2strategy == "r" || p2strategy == "p" || p2strategy == "s"
+	
+	if p1strategy == "p" and p2strategy == "s"
+		return game[1]
+	elsif p1strategy == "p" and p2strategy == "r"
+		return game[0]
+	elsif p1strategy == "p" and p2strategy == "p"
+		return "Tie"
+	elsif p1strategy == "r" and p2strategy == "p"
+		return game[1]
+	elsif p1strategy == "r" and p2strategy == "s"
+		return game[0]
+	elsif p1strategy == "r" and p2strategy == "r"
+		return "Tie"
+	elsif p1strategy == "s" and p2strategy == "p"
+		return game[0]
+	elsif p1strategy == "s" and p2strategy == "r"
+		return game[1]
+	elsif p1strategy == "s" and p2strategy == "s"
+		return "Tie"
+	#sources: http://codepad.org/Xbdvs7CQ
+end
+
+puts rps_game_winner([["Armando", "P"], ["Dave", "S"]])
+
+
 # Part4: Anagram
 def combine_anagrams(words)
 	words.group_by { |word| word.each_char.sort }.values #=>make a hash with each word as a key and an array of words related to each key as the values
