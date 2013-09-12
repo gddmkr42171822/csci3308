5b80fd142c714f99e0e5eef5f4420dc845b18238
latitude98 <latitude98@gmail.com>
2013-09-11 20:28:46 -0700

    finish diliverable 2

--- Lab2.rb
+++ Lab2.rb
@@ -35,13 +35,14 @@ end
 class WrongNumberOfPlayersError < StandardError ; end
 class NoSuchStrategyError < StandardError ; end
 
+
+
 def rps_game_winner(game)
 	raise WrongNumberOfPlayersError unless game.length == 2
 	p1strategy = game[0][1].downcase
 	p2strategy = game[1][1].downcase
 	raise NoSuchStrategyError unless p1strategy == "r" || p1strategy == "p" || p1strategy == "s"
 	raise NoSuchStrategyError unless p2strategy == "r" || p2strategy == "p" || p2strategy == "s"
-	
 	if p1strategy == "p" and p2strategy == "s"
 		return game[1]
 	elsif p1strategy == "p" and p2strategy == "r"
@@ -60,16 +61,39 @@ def rps_game_winner(game)
 		return game[1]
 	elsif p1strategy == "s" and p2strategy == "s"
 		return "Tie"
+	end
 	#sources: http://codepad.org/Xbdvs7CQ
+	#sources: https://github.com/epson121/Ruby/blob/master/Rock-Paper-Scissors.rb
+
 end
 
-puts rps_game_winner([["Armando", "P"], ["Dave", "S"]])
+# test game: puts rps_game_winner([["Armando", "p"], ["Dave", "S"]])
 
+def rps_tournament_winner(tournament)
 
+	if tournament[0][0].is_a?(String)
+		rps_game_winner(tournament)
+	else
+		return rps_tournament_winner([rps_tournament_winner(tournament[0]), rps_tournament_winner(tournament[1])])
+	end
+	#source: http://pastebin.com/GeXQx2FY
+end
+
+=begin test tournament: puts rps_tournament_winner([
+    [
+        [ ["Armando", "P"], ["Dave", "S"] ],
+        [ ["Richard", "R"],  ["Michael", "S"] ],
+    ],
+    [
+        [ ["Allen", "S"], ["Omer", "P"] ],
+        [ ["David E.", "R"], ["Richard X.", "P"] ]
+    ]
+])
+=end
 # Part4: Anagram
 def combine_anagrams(words)
-	words.group_by { |word| word.each_char.sort }.values #=>make a hash with each word as a key and an array of words related to each key as the values
+	print words.group_by { |word| word.each_char.sort }.values #=>make a hash with each word as a key and an array of words related to each key as the values
 	#source: http://stackoverflow.com/questions/16631961/how-can-i-simplify-or-clean-up-this-anagram-method
 end
 
-#=> combine_anagrams test: puts combine_anagrams(['cars', 'for', 'creams', 'fuck', 'racs', 'four', 'scar', 'potatoes','uckf', 'scream'])
+# test anagram: puts combine_anagrams(['cars', 'for', 'creams', 'fuck', 'racs', 'four', 'scar', 'potatoes','uckf', 'scream'])
