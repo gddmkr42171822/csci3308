fb597361ed951269d3f8ede21c3bd308f4b0fa1d
latitude98 <latitude98@gmail.com>
2013-09-14 09:17:22 -0700

    part2

---
+++ assign2.rb
@@ -0,0 +1,28 @@
+# Robert Werthman
+# Assignment 2
+
+
+#=> Part 1: Classes
+class Dessert
+	def initialize(name, calories)
+	
+	end
+	
+	def healthy?
+	
+	end
+	
+	def delicious
+	
+	end
+end
+
+class JellyBean < Dessert	
+	def initlize(name, calories, flavor)
+	
+	end
+	
+	def delicious?
+	
+	end
+end
