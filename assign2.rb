26d228b47267d059197a41f09fcd5e5a3b76f91b
latitude98 <latitude98@gmail.com>
2013-09-18 13:23:29 -0700

    complete

--- assign2.rb
+++ assign2.rb
@@ -98,4 +98,24 @@ module Enumerable
 end
 
 #=> Part 4: Blocks
+	
+	#sources:#sources: https://github.com/kevdashdev/saas-hw1/blob/master/ruby/part7.rb 
 		
+class CartesianProduct
+	include Enumerable
+	
+		def initialize(sequence1, sequence2)
+			@sequence1 = sequence1
+			@sequence2 = sequence2
+		end
+		
+		def each
+			unless @sequence1.empty? && @sequence2.empty? #=>don't execute if the sequences are empty
+			combination_sequence = []
+			@sequence1.each do |s1|
+				combination_sequence << @sequence2.each {|s2| yield [s1] << s2} #=>iterate through the first sequence while putting the second sequence in an array with the first		
+			end
+		end
+	end
+end
+
